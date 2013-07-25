38548680c9cd1c2c96284867f98aa864796418d1
indradhanush <indradhanush.gupta@gmail.com>
2013-07-25 16:19:20 +0530

    Using chrome-stable

--- .travis/scripts/install_chrome.sh
+++ .travis/scripts/install_chrome.sh
@@ -6,4 +6,4 @@ BASEDIR=$(readlink -f "$BASEDIR/..")
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update --force-yes
-sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-unstable
+sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-stable
