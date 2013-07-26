6101bcc54f022fb4ea5422fd388bb860593ca32b
indradhanush <indradhanush.gupta@gmail.com>
2013-07-26 19:12:06 +0530

    Corrected test suite.

--- Jasmine/spec/phoneSpec.js
+++ Jasmine/spec/phoneSpec.js
@@ -1,20 +1,11 @@
-describe("Test Outgoing Call", function() {
-    var callSpy;
-    beforeEach(function(){
-      callSpy = jasmine.createSpyObj('callSpy', ['login', ])    
-    });
-    var loginSpy = jasmine.createSpyObj("login",);
-    var onLoginSpy = jasmine.createSpy("onLogin");
+describe("Test Outgoing Call:", function() {
     var user1 = "end1130723173627";
     var pass1 = "testplivowebrtc";
     var dest = "end2130723173650@phone.plivo.com";
-    spyOn(loginSpy, 'login');
-    spyOn(onLoginSpy, 'onLogin')
-    loginSpy.login(user1, pass1)
-    
-    it("Tests Login", function(){
-      expect(loginSpy.login).toHaveBeenCalled();      
-      expect(onLoginSpy.onLogin).toHaveBeenCalled();
-
+        
+    it("Expects login() will be called", function(){
+      loginSpy = new login(user1, pass1);
+      spyOn(loginSpy, 'login');
+      expect(loginSpy).toHaveBeenCalled();      
     });    
 });
