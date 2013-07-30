0bdaa7013f7583120bd944b13aac1e77616bdc38
indradhanush <indradhanush.gupta@gmail.com>
2013-07-30 12:09:23 +0530

    Moving to selenium

---
+++ selenium_test/sim_call.py
@@ -0,0 +1,50 @@
+import os
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import ui
+
+chromedriver = "./chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+
+ENDPOINT = "end1130723173627" 
+PASSWORD = "testplivowebrtc" 
+DESTINATION = "end2130723173650@phone.plivo.com"
+
+class CallerEndpoint(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome(chromedriver)
+
+    def test_login(self):
+        driver = self.driver
+        driver.get("file:///home/dhanush/WebRTC-Monitor/caller.html")
+        #driver.wait_for_page_to_load("3000")
+        self.assertIn("Plivo Webphone Demo", driver.title)
+        wait = ui.WebDriverWait(driver, 30)
+        wait.until(lambda driver: driver.find_element_by_id("login_box"))
+        username = driver.find_element_by_id("username")
+        #wait.until(lambda driver: driver.find_element_by_id("password"))
+        passwd = driver.find_element_by_id("password")
+        username.send_keys(ENDPOINT)
+        passwd.send_keys(PASSWORD)
+        #passwd.submit()
+        #wait.until(lambda driver: driver.find_element_by_id("btn_login"))
+        driver.find_element_by_id("btn_login").click()
+        #driver.click("btn_login")
+        wait.until(lambda driver: driver.find_element_by_id(
+                                        "callcontainer").is_displayed())
+
+    # def test_something(self):
+    #    pass
+    def test_call(self):
+        driver = self.driver
+        wait = ui.WebDriverWait(driver, 30)
+        wait.until(lambda driver: driver.find_element_by_id(
+                                         "callcontainer").is_displayed())
+        print "Yeah!!!"
+        #destination = driver.find_element_by_id("to")
+        #destination.send_keys(DESTINATION)
+
+if __name__ == "__main__":
+        unittest.main()
