e5e8a0058b933f563ea09dd4f9478647d7a2054a
indradhanush <indradhanush.gupta@gmail.com>
2013-08-02 14:52:02 +0530

    Cleaned up unnecessary files.

--- .travis/scripts/install_chrome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname $0)
-BASEDIR=$(readlink -f "$BASEDIR/..")
-
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt-get update --force-yes
-sudo apt-get install -qq --force-yes xvfb imagemagick google-chrome-stable
